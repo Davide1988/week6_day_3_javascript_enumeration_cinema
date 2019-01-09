6b49c671cb0f04e8318a1e5faca4c3737c0b7475
Davide Venditti <davide_335@msn.com>
2019-01-09 14:47:23 +0000

    add second test

--- models/cinema.js
+++ models/cinema.js
@@ -6,4 +6,8 @@ Cinema.prototype.listFilm = function (films) {
   return films.map((film) => film.title)
 };
 
+Cinema.prototype.findByTitle = function (title) {
+  return result = this.films.filter((movie) => movie.title === title)
+};
+
 module.exports = Cinema;
